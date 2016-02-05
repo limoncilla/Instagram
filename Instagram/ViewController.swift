299644c2d9599a1e17c6e379c66c7d5e064cb81e
Limoncilla <limoncilla@Limoncillas-Mac.local>
2016-02-04 19:14:08 -0800

    lab commit

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures: [NSDictionary]?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+   
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let clientId = "Put your client id here"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.pictures = responseDictionary["data"] as![NSDictionary]
+                            
+                    }
+                }
+        });
+        task.resume()
+        
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +51,6 @@ class ViewController: UIViewController {
     }
 
 
+    
 }
 
